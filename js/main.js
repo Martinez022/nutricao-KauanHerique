a32f214b217d42bc8976a46b5d0d2212a77df065
Martinez022 <kauan.bobrowic@escola.pr.gov.br>
2024-11-26 00:27:36 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Kauan Nutricionista";
+
+var titulo = document.querySelector("h2");
+titulo.textContent = "Paciente Cadastrados";
+
+var paciente = document.querySelector("#primeiro-paciente");
